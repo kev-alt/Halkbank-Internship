5df6804fbe81f08acdcef2092a9df0bd1be7d4e0
kevser <kevser.altunbey@istanbulticaret.edu.tr>
2025-08-12 10:46:47 +0300

    Delete SQLExamples directory

--- SQLExamples/SQLQuery30.sql
+++
@@ -1,5 +0,0 @@
---CHARINDEX
-
-SELECT CHARINDEX('K', 'Kevser Altunbey',1)
-SELECT CHARINDEX('K', 'Kevser Altunbey',5)
-SELECT CHARINDEX('ser', 'Kevser Altunbey',1)
--- SQLExamples/SQLQuery34.sql
+++
@@ -1,9 +0,0 @@
---TRIM, LTRIM, RTRIM
-
-SELECT TRIM(' Kevser Altunbey ') --Kevser Altunbey
-
-SELECT LTRIM(' Kevser Altunbey ') --Kevser Altunbey 
-
-SELECT RTRIM(' Kevser Altunbey ') -- Kevser Altunbey
-
-SELECT LTRIM(RTRIM(' Kevser Altunbey ')) --Kevser Altunbey
