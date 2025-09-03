0fa67b6849bde3024840020aadd7ce5eed60b7a5
kevser <kevser.altunbey@istanbulticaret.edu.tr>
2025-09-03 14:10:34 +0300

    SQL

---
+++ SQLExamples/SQLQuery31.sql
@@ -0,0 +1,17 @@
+--CONCAT
+
+SELECT 'Kevser' + 'Altunbey'
+
+SELECT CONCAT('Kevser', ' ', 'Altunbey')
+
+SELECT CONCAT_WS(' ','Kevser', 'Altunbey')
+
+SELECT 
+USERNAME_ + ' ' + PASSWORD_ + ' ' + NAMESURNAME,
+* FROM USER_
+
+SELECT 
+CONCAT(USERNAME_, ' ', PASSWORD_, ' ', NAMESURNAME),
+CONCAT_WS(' ', USERNAME_, PASSWORD_, NAMESURNAME),
+* FROM USER_
+
---
+++ SQLExamples/SQLQuery34.sql
@@ -0,0 +1,9 @@
+--TRIM, LTRIM, RTRIM
+
+SELECT TRIM(' Kevser Altunbey ') --Kevser Altunbey
+
+SELECT LTRIM(' Kevser Altunbey ') --Kevser Altunbey 
+
+SELECT RTRIM(' Kevser Altunbey ') -- Kevser Altunbey
+
+SELECT LTRIM(RTRIM(' Kevser Altunbey ')) --Kevser Altunbey
